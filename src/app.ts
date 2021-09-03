abba7a2d38a79d4a6443558e07fd7d54d3281532
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-09-03 22:32:15 +0300

    init

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import config from 'config';
+
+const port = config.get('port') as number;
+const host = config.get('host') as string;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, host, () => {
+	console.info(`Server listening at http://${host}:${port}`);
+});
